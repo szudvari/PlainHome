f656c21606115bb4f2be78a0e258b3e502fba0cc
szudvari <udvarisz@yahoo.com>
2014-05-25 15:25:01 +0200

    logout finomítás

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
 session_start();
 include_once 'config.php';
 include_once 'html.php';
+include_once 'js.php';
 
 htmlHead($website['title'], $house['name']);
 webheader($_SESSION["admin"]);
+if (isset($_GET["logout"])) {
+    session_unset();
+    session_destroy();
+    popUp("Sikeresen kijelentkezett!");
+}
 htmlEnd();
