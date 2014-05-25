b2b15c63df9a6a04692ceb5f7cfb8d2720d4ef49
szudvari <udvarisz@yahoo.com>
2014-05-25 22:07:58 +0200

    addepo finomítás

--- adddepo.php
+++ adddepo.php
@@ -1,4 +1,5 @@
 <?php
+
 include_once 'functions.php';
 include_once 'db.php';
 include_once 'config.php';
@@ -9,11 +10,18 @@ $deposit['area'] = $_POST['area'];
 $deposit['residents'] = $_POST['residents'];
 $deposit['resident_name'] = $_POST['note'];
 $deposit['garage_area'] = $_POST['garage_area'];
-$deposit['area_ratio'] = str_replace(",",".",$_POST['area_ratio']);
-$deposit['garage_area_ratio'] = str_replace(",",".",$_POST['garage_area_ratio']);
-$deposit['watermeter'] = $_POST['watermeter'];
+$deposit['area_ratio'] = str_replace(",", ".", $_POST['area_ratio']);
+$deposit['garage_area_ratio'] = str_replace(",", ".", $_POST['garage_area_ratio']);
+if (isset($_POST['watermeter']) && ($_POST['watermeter'] == "on"))
+{
+    $deposit['watermeter'] = 1;
+}
+else
+{
+    $deposit['watermeter'] = 0;
+}
 
-//print_r ($deposit);        
+//print_r($deposit);
 $con= connectDb();
 insertDepoDb($deposit, $con);
 closeDb($con);
