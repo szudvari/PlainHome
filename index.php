1487a55d8f3418da9159b1fbdaeb57619c23f7e3
szudvari <udvarisz@yahoo.com>
2014-05-25 19:08:44 +0200

    Login utáni nyavaják már nem dobnak hibát

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php
+
 session_start();
 include_once 'config.php';
 include_once 'html.php';
 include_once 'js.php';
 htmlHead($website['title'], $house['name']);
-if (isset($_GET["logout"])) {
+if (isset($_GET["logout"]))
+{
     session_unset();
     session_destroy();
     $_SESSION["admin"] = NULL;
     popUp("Sikeresen kijelentkezett!");
 }
 webheader($_SESSION["admin"]);
-if (stripos($_SERVER['HTTP_REFERER'], "/login.php")) {
-    popUp("Sikeresen bejelentkezett mint \"".$_SESSION['user']."\"!");
+if (isset($_SERVER['HTTP_REFERER']))
+{
+    if (stripos($_SERVER['HTTP_REFERER'], "/login.php") && isset($_SERVER['user']))
+    {
+        popUp("Sikeresen bejelentkezett mint \"" . $_SESSION['user'] . "\"!");
+    }
 }
 htmlEnd();
