c9ba9eb648ddc39bc7b82cfc894ddd74cf2e86f1
AslanTheKing <aslantheking@AslanTheKings-iMac.local>
2014-06-01 14:06:20 +0200

    cim javitas

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $db['user'] = "root";
 $db['pass'] = "root";
 $db['charset'] = "utf8";
 
-$website['title'] = "PlainHome";
+$website['title'] = "PlainHouse";
 $website['version'] = "v0.0.0 alpha";
 
 $house['name'] = "Pöttyös utca 4.";
