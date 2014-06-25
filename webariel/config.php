689a3c4618e7c1a2138e8f21331c8b613719175c
Szabolcs Udvari <udvarisz@yahoo.com>
2014-06-25 09:15:09 +0200

    bugfix

--- webariel/config.php
+++ webariel/config.php
@@ -9,10 +9,11 @@ $website['title'] = "PlainHouse";
 $website['version'] = "1.0";
 
 $house['name'] = "Kossuth utca 3.";
-$house['infomail'] = "szudvari@gmail.com";
-$house['webpage'] = "http://users.ininet.hu/plainhouse";
+$house['infomail'] = "info@webariel.hu";
+$house['webpage'] = "http://webariel.hu/demo/plainhouse";
 $house['payment_day'] = 15;
 $house['deposit_id'] = 65;
+$house['bank_account'] = 'OTP Bank 12345678-12345678';
 
 if (!isset($_SESSION['admin'])) {
     $_SESSION['admin'] = NULL;
