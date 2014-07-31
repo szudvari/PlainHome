34a4b1aa1d3ff3a1ab7b51072f9daf582eaee614
AslanTheKing <aslantheking@AslanTheKings-iMac.local>
2014-07-31 18:32:57 +0200

    zod

--- db.php
+++ db.php
@@ -873,7 +873,7 @@ EOT;
 
             echo '<div class="row" style="font-size:0.8em;">';
             
-            echo '<div class="col-md-2"><i class="fa fa-square"></i>' . ' ' . $row['firstname'] . ' ' . $row['lastname'] . '</div>';
+            echo '<div class="col-md-2"><i class="fa fa-square" style="color: green;"></i>' . ' ' . $row['firstname'] . ' ' . $row['lastname'] . '</div>';
             sendMessageToUser($row['email'], $row['id']);
             echo '<div class="col-md-1" style="text-align:center;">' . $row['phone'] . '</div>';
             echo '<div class="col-md-1">' . $row['username'] . '</div>';
