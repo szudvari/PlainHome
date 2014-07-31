ce49ed92fb8811be0a8037b30a99f9b2271fc046
Szabolcs Udvari <udvarisz@yahoo.com>
2014-07-31 14:22:18 +0200

    Már tényleg! :)

--- html.php
+++ html.php
@@ -138,8 +138,9 @@ function addUser() {
                         </div>
                         <div class="form-group">
                             <label for="email">E-mail</label>
-                            <input type="text" id="email" name="email" class="form-control" data-bv-notempty="true"
-                                   data-bv-notempty-message="Az e-mail cím kitöltése kötelező!">
+                            <input type="email" id="email" name="email" class="form-control" data-bv-notempty="true"
+                                   data-bv-notempty-message="Az e-mail cím kitöltése kötelező!"
+                                       data-bv-emailaddress-message="Hibás e-mail cím!">
 
                         </div>
                         <div class="form-group">
